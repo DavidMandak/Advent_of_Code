d3671baedb2f175a1cb6cad2cb29c9bbd501eaae
David Mandak <dejf.mandak@gmail.com>
2025-12-05 16:29:12 +0100

    Swap

--- 2025/Programs/day05.py
+++ 2025/Programs/day05.py
@@ -12,5 +12,39 @@ for i in ids:
             total_1 += 1
             break
 
+total_2 = 0
+fresh = []
+for r in ranges:
+    r = [r]
+    while r:
+        s, e = r.pop()
+        for sf, ef in fresh:
+
+            if s < sf:
+                if e >= sf:
+                    if e <= ef:
+                        e = sf-1
+                    else:
+                        e = sf-1
+                        r.append(ef+1, e)
+                else:
+                    pass
+            else:
+                if s <= ef:
+                    if e > ef:
+                        s = ef+1
+                    else:
+                        s = None
+                        break
+                else:
+                    pass
+
+            if s is not None:
+                fresh.append(s, e)
+                            
+for sf, ef in fresh:
+    total_2 += ef-sf+1
+
 print(total_1)
+print(total_2)
 print(time.time()-t)
