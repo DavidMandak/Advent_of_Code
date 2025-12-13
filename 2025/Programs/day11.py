80922c55c1d8657f6fff638aff6f5affa4467f89
David Mandak <dejf.mandak@gmail.com>
2025-12-13 20:30:19 +0100

    Day 11 part 2 but it's very slow

--- 2025/Programs/day11.py
+++ 2025/Programs/day11.py
@@ -3,14 +3,14 @@ from collections import defaultdict
 
 
 def main() -> None:
-    lines = [line.split() for line in open("2025/Inputs/input11.txt").read().splitlines()]
+    lines = [line.split() for line in open("2025/Inputs/test.txt").read().splitlines()]
     t = time.time()
 
     nodes_1 = setup_1(lines)
     total_1 = solve_1(nodes_1)
 
     nodes_2 = setup_2(lines)
-    total_2 = solve_2(nodes_1, len(lines))
+    total_2 = solve_2(nodes_1, nodes_2, len(lines))
 
     print(total_1)
     print(total_2)
@@ -26,6 +26,7 @@ def setup_1(lines: list) -> dict:
 
 
 def solve_1(nodes: dict) -> int:
+    return
     total_1 = dfs_1("you", nodes)
 
     return total_1
@@ -47,19 +48,47 @@ def setup_2(lines: list) -> dict:
     return nodes
 
 
-def solve_2(nodes: dict, length: int) -> int:
-    total_2 = dfs_2("svr", nodes, [0]*length)
+def solve_2(nodes_1: dict, nodes_2: dict, length: int) -> int:
+    total_2 = 1
+
+    paths = defaultdict(int)
+    dfs_2("svr", nodes_1, paths)
+    total_2 *= dfs_2_back("fft", nodes_2, paths)+1
+    #print(paths)
+
+    paths = defaultdict(int)
+    dfs_2("fft", nodes_1, paths)
+    total_2 *= dfs_2_back("dac", nodes_2, paths)+1
+    #print(paths)
+    print()
+
+    paths = defaultdict(int)
+    dfs_2("dac", nodes_1, paths)
+    print(paths)
+    total_2 *= dfs_2_back("out", nodes_2, paths)+1
+
+    return total_2
 
 
-def dfs_2(curr: str, nodes: dict, visited: list) -> int:
-    if curr in visited:
-        raise Exception
+def dfs_2(curr: str, nodes: dict, paths: dict) -> None:
+    paths[curr] += 1
+    if paths[curr] != 1:
+        return
     elif curr == "out":
-        return 0
+        return
     
-    visited.append(curr)
-    return sum([dfs_2(node, nodes, visited.copy()) for node in nodes[curr]])
+    for node in nodes[curr]:
+        dfs_2(node, nodes, paths)
 
+
+def dfs_2_back(curr: str, nodes: dict, paths: dict) -> int:
+    if paths[curr]:
+        a = [dfs_2_back(node,  nodes, paths) for node in nodes[curr]]
+        print(curr)
+        print(a)
+        print(paths[curr]+sum(a)-1)
+        return paths[curr]+sum(a)-1
+    return 0
 
 
 if __name__ == "__main__":
